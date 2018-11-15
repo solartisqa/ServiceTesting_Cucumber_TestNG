107e86a8bdff005f63a9e248b9c7dd57194e608b
solartisqa <sasirekha_a@solartis.com>
2018-11-15 14:58:25 +0530

    java update

--- src/main/java/com/solartis/test/listeners/LogListener.java
+++ src/main/java/com/solartis/test/listeners/LogListener.java
@@ -6,7 +6,7 @@ import org.apache.log4j.PropertyConfigurator;
 import com.solartis.test.testRunners.MainTestRunner;
 
 
-public class LogListener implements Listener
+public class LogListener /*implements Listener*/
 {
 	private static Logger Log;
 	static
@@ -16,117 +16,117 @@ public class LogListener implements Listener
 		Log = Logger.getLogger(MainTestRunner.class);
 	}
 	
-	@Override
+
 	public void beforeLoadSampleRequest()
 	{
 		//System.out.println("beforeLoadSampleRequest");
 		Log.info("beforeLoadSampleRequest");
 	}
 
-	@Override
+
 	public void afterLoadSampleRequest() 
 	{
 		//System.out.println("afterLoadSampleRequest");
 		Log.info("afterLoadSampleRequest");
 	}
 
-	@Override
+
 	public void beforePumpDataToRequest()
 	{
 		//System.out.println("beforePumpDataToRequest");
 		Log.info("beforePumpDataToRequest");
 	}
 
-	@Override
+
 	public void afterPumpDataToRequest() 
 	{
 		//System.out.println("afterPumpDataToRequest");
 		Log.info("afterPumpDataToRequest");
 	}
-	@Override
+
 	public void beforeRequestToString()
 	{
 		//System.out.println("beforeRequestToString");
 		Log.info("beforeRequestToString");
 	}
 
-	@Override
+
 	public void afterRequestToString() 
 	{
 		//System.out.println("afterRequestToString");
 		Log.info("afterRequestToString");
 	}
 
-	@Override
+
 	public void beforeAddHeaders(String Token)
 	{
 		//System.out.println("beforeAddHeaders");
 		Log.info("beforeAddHeaders");
 	}
 
-	@Override
+
 	public void afterAddHeaders(String Token) 
 	{
 		//System.out.println("afterAddHeaders");
 		Log.info("afterAddHeaders");
 	}
 
-	@Override
+
 	public void beforeSendAndReceiveData()
 	{
 		//System.out.println("beforeSendAndReceiveData");
 		Log.info("beforeSendAndReceiveData");
 	}
 
-	@Override
+
 	public void afterSendAndReceiveData() 
 	{
 		//System.out.println("afterSendAndReceiveData");
 		Log.info("afterSendAndReceiveData");
 	}
 	
-	@Override
+
 	public void beforeResponseToString()
 	{
 		//System.out.println("beforeResponseToString");
 		Log.info("beforeResponseToString");
 	}
 
-	@Override
+
 	public void afterResponseToString() 
 	{
 		//System.out.println("afterResponseToString");
 		Log.info("afterResponseToString");
 	}
 
-	@Override
+
 	public void beforeSendResponseDataToFile()
 	{
 		//Log.info("beforeSendResponseDataToFile");
 	}
 
-	@Override
+
 	public void afterSendResponseDataToFile() 
 	{
 		//System.out.println("afterSendResponseDataToFile");
 		Log.info("afterSendResponseDataToFile");
 	}
 
-	@Override
+
 	public void beforeCompareFunction() 
 	{
 		//System.out.println("beforeCompareFunction");
 		Log.info("beforeCompareFunction");
 	}
 
-	@Override
+
 	public void afterCompareFunction() 
 	{
 		//System.out.println("afterCompareFunction");
 		Log.info("afterCompareFunction");
 	}
 
-	@Override
+
 	public void onError(Throwable e)
 	{
 		//System.out.println("Printed by Listener");
@@ -136,25 +136,25 @@ public class LogListener implements Listener
 		e.printStackTrace();
 	}
 
-	@Override
+
 	public void beforeTokenGenerator() {
 		// TODO Auto-generated method stub
 		Log.info("beforeTokenGenerator");
 	}
 
-	@Override
+
 	public void afterTokenGeneratior() {
 		// TODO Auto-generated method stub
 		Log.info("afterTokenGeneratior");
 	}
 
-	@Override
+
 	public void beforedifferrence() {
 		// TODO Auto-generated method stub
 		Log.info("beforedifference");
 	}
 
-	@Override
+
 	public void afterdifferrence() {
 		// TODO Auto-generated method stub
 		Log.info("afterdifference");
